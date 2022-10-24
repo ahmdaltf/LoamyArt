74fb033fe1feb4550f1f3e115a1e1432bc82a04b
xheroverse <ahmadaltaf0571@gmail.com>
2022-10-24 10:44:31 +0500

    random Experiments

--- noise.js
+++ noise.js
@@ -5,10 +5,12 @@ const palettes = require('nice-color-palettes')
 
 const settings = {
   dimensions: [ 2048, 2048 ],
-  animate: true
+  animate: true,
+  duration: 10
 };
 
 const palette = random.pick(palettes)
+const text = ['◠', '◡', '◝', '◜', '◞', '◟']
 
 const createGrid = () => {
   let count = 25
@@ -17,22 +19,23 @@ const createGrid = () => {
     for(let j=0;j<count;j++){
       let u = count <=1 ? 0.5 : i/(count-1)
       let v = count <=1 ? 0.5 : j/(count-1)
-      const radius = Math.max(0.02, random.gaussian() * 0.01)
+      const radius = Math.max(0.02, random.gaussian() * 0.1)
       points.push({
         position: [u,v],
         radius,
         color: random.pick(palette),
+        charSet: random.pick(text)
       })
     }
   }
   return points
 }
 const points = createGrid()
-const margin = 400
+const margin = 200
 
 const sketch = () => {
   return ({ context, width, height, frame }) => {
-    context.fillStyle = '#fefefe'
+    context.fillStyle = "black"
     context.fillRect(0, 0, width, height)
 
     points.forEach((data) => {
@@ -40,6 +43,7 @@ const sketch = () => {
         position,
         color,
         radius,
+        charSet
       } = data
 
       const [u,v] = position
@@ -47,17 +51,16 @@ const sketch = () => {
       const x = lerp(margin, width-margin, u)
       const y = lerp(margin, height-margin, v)
       
-      const randNoise = random.noise2D(x, y + frame * 10, 0.01)
-      const rotation = randNoise * Math.PI * 0.1
+      const randNoise = random.noise2D(u + frame * 15, v + frame * 15, 0.001, 1)
+      const rotation = randNoise * (Math.PI/3) * 0.1
       
       context.save()
       context.beginPath()
-      context.arc(x, y, radius*width, 0, Math.PI*2)
-      context.fillStyle = "black"
+      context.fillStyle = "white"
       context.translate(x,y)
-      context.rotate(rotation)
-      context.font = `${(radius * width) + 50}px "Arial"`
-      context.fillText('⸅',0,0)
+      context.rotate(randNoise)
+      context.font = `${(0.05 * width) + 50}px "Arial"`
+      context.fillText("⸅",0,0) //⸅
       context.restore()
     })
   };
