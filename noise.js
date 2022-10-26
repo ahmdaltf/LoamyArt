fbf8288cd6c3cea682da629f6b2eb1c9006c03e2
xheroverse <ahmadaltaf0571@gmail.com>
2022-10-26 11:14:40 +0500

    Minor Changes

--- noise.js
+++ noise.js
@@ -1,44 +1,62 @@
 const canvasSketch = require('canvas-sketch')
-const { lerp } = require('canvas-sketch-util/math')
+const { lerp, degToRad } = require('canvas-sketch-util/math')
 const random = require('canvas-sketch-util/random')
 const palettes = require('nice-color-palettes')
 
 const settings = {
   dimensions: [ 2048, 2048 ],
-  animate: true,
-  duration: 10
+  animate: false,
+  duration: 20
 };
 
-const palette = random.pick(palettes)
+const palette = ["#f8f9fa","#e9ecef","#dee2e6","#ced4da","#adb5bd","#6c757d","#495057","#343a40","#212529"]
 const text = ['◠', '◡', '◝', '◜', '◞', '◟']
 
-const createGrid = () => {
-  let count = 25
-  let points = []
-  for(let i=0;i<count;i++){
-    for(let j=0;j<count;j++){
-      let u = count <=1 ? 0.5 : i/(count-1)
-      let v = count <=1 ? 0.5 : j/(count-1)
-      const radius = Math.max(0.02, random.gaussian() * 0.1)
-      points.push({
+const createLines = () => {
+  let lineX = 50
+  let lineY = 40
+  let linePoints = []
+  for(let i=0;i<lineX;i++){
+    for(let j=0;j<lineY;j++){
+      let u = lineX <=1 ? 0.5 : i/(lineX-1)
+      let v = lineY <=1 ? 0.5 : j/(lineY-1)
+      linePoints.push({
         position: [u,v],
-        radius,
         color: random.pick(palette),
         charSet: random.pick(text)
       })
     }
   }
-  return points
+  return linePoints
 }
-const points = createGrid()
-const margin = 200
+const linePoints = createLines()
+const createGrid = () => {
+  let gridX = 50
+  let gridY = 25
+  let gridPoints = []
+  for(let i=0;i<gridX;i++){
+    for(let j=0;j<gridY;j++){
+      let u = gridX <=1 ? 0.5 : i/(gridX-1)
+      let v = gridY <=1 ? 0.5 : j/(gridY-1)
+      gridPoints.push({
+        position: [u,v],
+        color: random.pick(palette),
+        charSet: random.pick(text)
+      })
+    }
+  }
+  return gridPoints
+}
+
+const gridPoints = createGrid()
+const margin = 50
 
 const sketch = () => {
   return ({ context, width, height, frame }) => {
-    context.fillStyle = "black"
+    context.fillStyle = 'black'
     context.fillRect(0, 0, width, height)
 
-    points.forEach((data) => {
+    linePoints.forEach((data) => {
       const {
         position,
         color,
@@ -48,19 +66,46 @@ const sketch = () => {
 
       const [u,v] = position
 
+      const x = lerp(0, width-margin, u)
+      const y = lerp(margin, height-margin, v)
+      
+      // const randNoise = random.noise2D(x + frame * 50 , y + frame * 50, 0.01)
+      // const rotation = randNoise * Math.PI * 0.1
+
+      context.save()
+      context.beginPath()
+      context.lineWidth = 10
+      context.translate(x,y)
+      context.moveTo(x, y)
+      context.lineTo(x+200, y)
+      context.strokeStyle = 'white'
+      context.stroke()
+      context.restore()
+    })
+    
+    gridPoints.forEach((data) => {
+      const {
+        position,
+        color,
+        charSet
+      } = data
+
+      const [u,v] = position
+
       const x = lerp(margin, width-margin, u)
       const y = lerp(margin, height-margin, v)
       
-      const randNoise = random.noise2D(u + frame * 15, v + frame * 15, 0.001, 1)
-      const rotation = randNoise * (Math.PI/3) * 0.1
-      
+      // const randNoise = random.noise2D(x + frame * 50 , y + frame * 50, 0.01)
+      // const rotation = randNoise * Math.PI * 0.1
+
       context.save()
       context.beginPath()
-      context.fillStyle = "white"
       context.translate(x,y)
-      context.rotate(randNoise)
-      context.font = `${(0.05 * width) + 50}px "Arial"`
-      context.fillText("⸅",0,0) //⸅
+      if(Math.random() > 0.7){
+      context.fillStyle = 'white'
+      context.arc(x, y, 25, 0, 360)
+      context.fill()
+      }
       context.restore()
     })
   };
--- velocityGrid.js
+++ velocityGrid.js
@@ -1,6 +1,6 @@
 const canvasSketch = require('canvas-sketch')
 const random = require('canvas-sketch-util/random')
-const { lerp } = require('canvas-sketch-util/math')
+const { lerp, degToRad } = require('canvas-sketch-util/math')
 import {Pane}  from 'tweakpane'
 const palettes = require('nice-color-palettes')
 
@@ -12,8 +12,8 @@ const settings = {
 }
 
 const params = {
-  rows: 5,
-  cols: 2,
+  rows: 20,
+  cols: 28,
 }
 
 const palette = ['#03071e', '#370617', '#6a040f', '#9d0208', '#d00000', '#dc2f02', '#e85d04', '#f48c06', '#faa307', '#fbb539']
@@ -23,6 +23,26 @@ const timeFunc = () =>
   {setInterval(function() {if(params.cols != 50) {params.cols +=1}}, 400)}
 
 const myTimeout = setTimeout(timeFunc, 5000);
+const countX = params.rows
+const countY = params.cols
+
+const createGrid = () => {
+  const points = []
+  for(let i = 0; i<countX; i++){
+    for(let j = 0; j<countY; j++){
+      const u = i/(countX-1)
+      const v = j/(countY-1)
+      points.push({
+        position: [u, v],
+        color: random.pick(palette)
+      })
+    }
+  }
+  return points
+}
+
+const points = createGrid()
+const margin = 400
 
 const sketch = () => {
   
@@ -31,26 +51,6 @@ const sketch = () => {
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
     
-    const countX = params.rows
-    const countY = params.cols
-
-    const createGrid = () => {
-      const points = []
-      for(let i = 0; i<countX; i++){
-        for(let j = 0; j<countY; j++){
-          const u = i/(countX-1)
-          const v = j/(countY-1)
-          points.push({
-            position: [u, v],
-            color: random.pick(palette)
-          })
-        }
-      }
-      return points
-    }
-    
-    const points = createGrid()
-    const margin = 400
     points.forEach((data) => {
       const {
         position,
@@ -61,17 +61,18 @@ const sketch = () => {
       let x = lerp(margin, width-margin, u)
       let y = lerp(margin, height-margin, v)
 
-      const randNoise = random.noise2D(x + frame * 15, y + frame * 15, 0.001)
-      const angle = randNoise * Math.PI * 0.1
+      const randNoise = random.noise3D(x , y ,frame * 50, 0.001)
+      const angle = randNoise * degToRad(360)
 
       context.save()
       context.translate(x, y)
       context.beginPath()
       context.rotate(angle)
-      context.lineWidth = random.range(10, 30)
-      // context.lineWidth = 15
-      context.moveTo(0 , 0)
-      context.lineTo(0 + 100 , 0)
+      // context.lineWidth = random.range(10, 30)
+      context.lineWidth = 5
+      context.rect(0, 0, 10, 10)
+      // context.moveTo(0 , 0)
+      // context.lineTo(0 + 100 , 0)
       context.strokeStyle = color
       context.stroke()
       context.restore()
